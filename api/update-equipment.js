fbd3c9b9e556c50f90d6638e223fed745d022006
priscilacaputo <prisci.caputo.pc@gmail.com>
2026-07-16 15:49:52 -0300

    Add files via upload

--- api/update-equipment.js
+++ api/update-equipment.js
@@ -13,6 +13,8 @@ const SECTION_CONFIG = {
   aac:         { file: 'assets/js/aac-data.js',         varName: 'AAC_DATA' },
   ecas:        { file: 'assets/js/ecas-data.js',        varName: 'ECAS_DATA' },
   otros:       { file: 'assets/js/otros-data.js',       varName: 'OTROS_DATA' },
+  estados:     { file: 'assets/js/estado-equipos-data.js', varName: 'ESTADO_OVERRIDES' },
+  tareas:      { file: 'assets/js/tareas-data.js',      varName: 'TAREAS_DATA' },
 };
 
 function githubRequest(method, path, token, body) {
