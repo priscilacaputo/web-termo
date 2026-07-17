453e72b0c38301b2b2f89465ed0be3feec705439
priscilacaputo <prisci.caputo.pc@gmail.com>
2026-07-17 16:03:25 -0300

    Admin: actualizar estados [2026-07-17]

--- assets/js/estado-equipos-data.js
+++ assets/js/estado-equipos-data.js
@@ -18,11 +18,12 @@ const ESTADO_OVERRIDES = [
     "hidrolavadoManual": "2026-07-17"
   },
   {
-    "equipo": "AAC9428",
+    "equipo": "AAC9427",
     "estadoManual": null,
     "comentarios": [],
     "hidrolavadoManual": "2026-07-17"
   }
 ];
+
 
 
